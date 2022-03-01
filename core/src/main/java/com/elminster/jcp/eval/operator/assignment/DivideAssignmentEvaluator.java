2cbfae2318e3f1bb2ea610d4a1cd3b62bb0ad798
jgu <jinglei.gu@ca-nio.com>
2022-03-01 15:30:13 +0800

    add divide assignment evaluator and mod assignment evaluator

---
+++ core/src/main/java/com/elminster/jcp/eval/operator/assignment/DivideAssignmentEvaluator.java
@@ -0,0 +1,29 @@
+package com.elminster.jcp.eval.operator.assignment;
+
+import com.elminster.jcp.ast.Node;
+import com.elminster.jcp.ast.expression.LiteralExpression;
+import com.elminster.jcp.ast.expression.base.Divide;
+import com.elminster.jcp.ast.expression.base.Plus;
+import com.elminster.jcp.ast.expression.literal.IntLiteral;
+import com.elminster.jcp.eval.context.EvalContext;
+import com.elminster.jcp.eval.data.Data;
+import com.elminster.jcp.eval.operator.arithmetic.DivideEvaluator;
+import com.elminster.jcp.eval.operator.arithmetic.MultiEvaluator;
+
+public class DivideAssignmentEvaluator extends AssignmentEvaluator {
+
+    public DivideAssignmentEvaluator(Node astNode) {
+        super(astNode);
+    }
+
+    @Override
+    protected Data doEval(EvalContext evalContext, Data eval, Data variable) {
+        checkOperands(eval, variable);
+
+        DivideEvaluator divideEvaluator = new DivideEvaluator(new Divide(
+                new LiteralExpression(IntLiteral.of(((Integer) variable.get()))),
+                new LiteralExpression(IntLiteral.of((Integer) eval.get())))
+        );
+        return divideEvaluator.eval(evalContext);
+    }
+}
---
+++ core/src/main/java/com/elminster/jcp/eval/operator/assignment/ModAssignmentEvaluator.java
@@ -0,0 +1,29 @@
+package com.elminster.jcp.eval.operator.assignment;
+
+import com.elminster.jcp.ast.Node;
+import com.elminster.jcp.ast.expression.LiteralExpression;
+import com.elminster.jcp.ast.expression.base.Divide;
+import com.elminster.jcp.ast.expression.base.Mod;
+import com.elminster.jcp.ast.expression.literal.IntLiteral;
+import com.elminster.jcp.eval.context.EvalContext;
+import com.elminster.jcp.eval.data.Data;
+import com.elminster.jcp.eval.operator.arithmetic.DivideEvaluator;
+import com.elminster.jcp.eval.operator.arithmetic.ModEvaluator;
+
+public class ModAssignmentEvaluator extends AssignmentEvaluator {
+
+    public ModAssignmentEvaluator(Node astNode) {
+        super(astNode);
+    }
+
+    @Override
+    protected Data doEval(EvalContext evalContext, Data eval, Data variable) {
+        checkOperands(eval, variable);
+
+        ModEvaluator modEvaluator = new ModEvaluator(new Mod(
+                new LiteralExpression(IntLiteral.of(((Integer) variable.get()))),
+                new LiteralExpression(IntLiteral.of((Integer) eval.get())))
+        );
+        return modEvaluator.eval(evalContext);
+    }
+}
